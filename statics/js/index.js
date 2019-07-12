c98c0603edcf4f3b4c8f695896a4d6f91e7e6689
Laila Ferreira <laila.orsayferreira@gmail.com>
2019-07-11 23:15:30 -0300

    adição carrossel e seção about-solucoes

---
+++ statics/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+		$('.thing').slick({
+			dots: true,
+			arrows: false,
+			autoplay: false,
+			vertical: true,
+        	centerMode: true,
+			autoplaySpeed: 1000,
+			infinite: true
+		})
+	});
